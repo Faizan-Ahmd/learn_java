ab19430cbd5d6792b3aadf3f42552faddef1e259
Faizan Ahmad <faizanahmad1033@gmail.com>
2023-04-26 12:00:52 +0500

    local instance and static variables.

--- variables.java
+++ variables.java
@@ -1,9 +1,9 @@
 public class variables{
-    static int m=100;
+    static int m=100;//static variable
     void method(){
-        int n=90;
+        int n=90;//local variable
     }
     public static void main(String[] args){
-        int data=50;
+        int data=50;//instance variable
     }
 }
