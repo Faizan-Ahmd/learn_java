b7597706b1c1bb444e1152d0febe89702113d6d9
Faizan Ahmad <faizanahmad1033@gmail.com>
2023-04-26 13:08:33 +0500

    short data type

---
+++ Short.java
@@ -0,0 +1,8 @@
+public class Short{
+    public static void main(String args[]){
+        //short data type
+        short number=32767;
+        //short data type is 16 bit signed integer whose value ranges from -32768 to 32767
+        System.out.println(number);
+    }
+}
