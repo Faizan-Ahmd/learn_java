24eb6cab7e027bb78e0ffdef4e30dd65340e48f4
Faizan Ahmad <faizanahmad1033@gmail.com>
2023-04-26 12:57:28 +0500

    Byte data type

---
+++ Byte.java
@@ -0,0 +1,7 @@
+public class Byte{
+    public static void main(String[] args){
+        byte number=-128;//byte data type
+        //byte data type is 8bit signed two's complement integer it value range lies between -128 to 127(inclusive)
+        System.out.println(number);
+    }
+}
